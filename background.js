5ed568162c1de9a4867818fac40d9d1d22174c59
BH_Lin <binghuanlin@gmail.com>
2014-04-10 01:47:18 +0800

    Symptom:    first checkin. Bug ID: Root Cause: Solution: Note:

---
+++ background.js
@@ -0,0 +1,37 @@
+var DBG = true;
+
+var currentTime = "";
+var time;
+function checkTime()
+{
+
+    time = (new Date());
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+
+    if(hours < 10) {
+        currentTime = "0" +  hours;
+    } else {
+        currentTime = hours;
+    }
+
+    //currentTime += ":";
+
+    if(minutes < 10) {
+        currentTime += "0" +  minutes;
+    } else {
+        currentTime += minutes;
+    }
+
+    chrome.browserAction.setBadgeText({text:currentTime});
+    //chrome.browserAction.setIcon({path:currentWeatherInfo.weatherIconUrl[0].value});  
+}
+
+function scheduleRequest() {
+	var reqeustInterval = 1000 * 30;
+	console.log("Scheduling request...");
+    setInterval(checkTime, reqeustInterval);
+}
+
+checkTime();
+scheduleRequest();
